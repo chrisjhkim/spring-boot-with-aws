ca7aa24334d7ff9b0581a6616eced09343dc447d
chrisjhkim <chris1108g@gmail.com>
2023-12-02 21:13:51 +0900

    Posts CRU(D)

--- src/main/java/com/chrisjhkim/book/springboot/service/PostsService.java
+++ src/main/java/com/chrisjhkim/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.chrisjhkim.book.springboot.service;
 
 import com.chrisjhkim.book.springboot.domain.posts.Posts;
 import com.chrisjhkim.book.springboot.domain.posts.PostsRepository;
+import com.chrisjhkim.book.springboot.web.dto.PostsListResponseDto;
 import com.chrisjhkim.book.springboot.web.dto.PostsResponseDto;
 import com.chrisjhkim.book.springboot.web.dto.PostsSaveRequestDto;
 import com.chrisjhkim.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -35,5 +39,12 @@ public class PostsService {
 				.orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
 
 		return new PostsResponseDto(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public List<PostsListResponseDto> findAllDesc(){
+		return postsRepository.findAllDesc().stream()
+				.map(PostsListResponseDto::new)
+				.collect(Collectors.toList());
 	}
 }
--- src/main/java/com/chrisjhkim/book/springboot/web/IndexController.java
+++ src/main/java/com/chrisjhkim/book/springboot/web/IndexController.java
@@ -1,13 +1,36 @@
 package com.chrisjhkim.book.springboot.web;
 
+import com.chrisjhkim.book.springboot.service.PostsService;
+import com.chrisjhkim.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
+@Slf4j
 public class IndexController {
-
+	private final PostsService postsService;
 	@GetMapping("/")
-	public String index(){
+	public String index(Model model){
+
+		model.addAttribute("posts", postsService.findAllDesc());
 		return "index";
+	}
+
+	@GetMapping("/posts/save")
+	public String postsSave(){
+		return "posts-save";
+	}
+
+	@GetMapping("/posts/update/{id}")
+	public String postsUpdate(@PathVariable Long id, Model model){
+		log.debug("IndexController.postsUpdate");
+		PostsResponseDto dto = postsService.findById(id);
+		model.addAttribute("post", dto);
+		return "posts-update";
 	}
 }
