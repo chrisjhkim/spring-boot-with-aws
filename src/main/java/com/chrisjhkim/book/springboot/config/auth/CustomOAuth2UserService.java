67ab225a8a11c7d148954a89640b9e63b05c77d3
chrisjhkim <chris1108g@gmail.com>
2023-12-07 01:12:32 +0900

    구글 로그인 연동

--- src/main/java/com/chrisjhkim/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/chrisjhkim/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -1,15 +1,21 @@
 package com.chrisjhkim.book.springboot.config.auth;
 
+import com.chrisjhkim.book.springboot.config.auth.dto.OAuthAttributes;
+import com.chrisjhkim.book.springboot.config.auth.dto.SessionUser;
+import com.chrisjhkim.book.springboot.domain.user.User;
 import com.chrisjhkim.book.springboot.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
@@ -22,9 +28,37 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 		OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
 		OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
-		String registrationId = userRequest.getClientRegistration().getRegistrationId();
-		String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+		// 현재 로그인 진행 중인 서비스를 구분하는 코드. (구글/네이버 등을 구분)
+		String registrationId = userRequest
+				.getClientRegistration()
+				.getRegistrationId();
+		// PK 값 같은 개념
+		String userNameAttributeName = userRequest
+				.getClientRegistration()
+				.getProviderDetails()
+				.getUserInfoEndpoint()
+				.getUserNameAttributeName();
 
+		// OAuth2UserService 를 통해 가져온 OAuth2User 의 attribute 담을 DTO
+		OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
+		User user = saveOrUpdate(attributes);
+
+		httpSession.setAttribute(
+				"user",
+				new SessionUser(user)  // 세션에 사용자 정보를 저장하기 위한 DTO
+		);
+
+		return new DefaultOAuth2User(
+				Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+				attributes.getAttributes(),
+				attributes.getNameAttributeKey());
+	}
+
+	private User saveOrUpdate(OAuthAttributes attributes) {
+		User user = userRepository.findByEmail(attributes.getEmail())
+				.map(user1 -> user1.update(attributes.getName(), attributes.getPicture()))
+				.orElse(attributes.toEntity());
+		return userRepository.save(user);
 	}
 }
--- src/main/java/com/chrisjhkim/book/springboot/web/IndexController.java
+++ src/main/java/com/chrisjhkim/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.chrisjhkim.book.springboot.web;
 
+import com.chrisjhkim.book.springboot.config.auth.dto.SessionUser;
 import com.chrisjhkim.book.springboot.service.PostsService;
 import com.chrisjhkim.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 @Slf4j
 public class IndexController {
 	private final PostsService postsService;
+	private final HttpSession httpSession;
+
 	@GetMapping("/")
 	public String index(Model model){
-
 		model.addAttribute("posts", postsService.findAllDesc());
+
+		SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+		if ( user != null ) {
+			model.addAttribute("userName", user.getName());
+		}
+
 		return "index";
 	}
 
