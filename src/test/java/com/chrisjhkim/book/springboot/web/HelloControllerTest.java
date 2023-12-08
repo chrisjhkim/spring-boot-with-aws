11bb41b16fa93b0cdab121193d1bbf0a17726fd9
chrisjhkim <chris1108g@gmail.com>
2023-12-09 01:08:18 +0900

    Test 환경 변경

--- src/test/java/com/chrisjhkim/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/chrisjhkim/book/springboot/web/HelloControllerTest.java
@@ -1,19 +1,33 @@
 package com.chrisjhkim.book.springboot.web;
 
 
+import com.chrisjhkim.book.springboot.config.auth.SecurityConfig;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+/**<p>
+ * {@code @WebMvcTest} 는 WebSecurityConfigurerAdapter, WebMvcConfigurer 를 비롯한 @ControllerAdvice, @Controller 를 읽는다.
+ *  {@code @Repository}, @Service, @Component 등은 대상이 아니다.
+ * </p>
+ */
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(
+		controllers = HelloController.class,
+		excludeFilters = {
+				@ComponentScan.Filter(
+						type = FilterType.ASSIGNABLE_TYPE,
+						classes = SecurityConfig.class)})
 public class HelloControllerTest {
 
 	@Autowired
@@ -22,6 +36,7 @@ public class HelloControllerTest {
 	/**
 	 * Hello 가 return 된다
 	 */
+	@WithMockUser(roles = "USER")
 	@Test
 	public void returnsHello() throws Exception {
 		String hello = "hello";
@@ -31,6 +46,7 @@ public class HelloControllerTest {
 				.andExpect(content().string(hello));
 	}
 
+	@WithMockUser(roles = "USER")
 	@Test
 	public void returnHelloDto() throws Exception {
 		String name = "hello";
